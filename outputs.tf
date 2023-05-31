eea1f98119c5bb7039308e1d8ef7f2e44dd3306a
Thimo Limpert (q.beyond AG) <thimo.limpert@qbeyond.de>
2023-05-31 12:01:57 +0200

    add test display_name and description from policy_set

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "resource_group_policy_assignment" {
+  value = azurerm_resource_group_policy_assignment.this
+  description = "The `azurerm_resource_group_policy_assignment` object, if scope was a `resource_group`."
+}
