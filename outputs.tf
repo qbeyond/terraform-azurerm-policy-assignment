f679657860b901dd24af7b89746e63edf3127274
Thimo Limpert (q.beyond AG) <thimo.limpert@qbeyond.de>
2023-06-09 10:15:14 +0200

    fix validation

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "resource_group_policy_assignment" {
-  value = azurerm_resource_group_policy_assignment.this
+  value       = azurerm_resource_group_policy_assignment.this
   description = "The `azurerm_resource_group_policy_assignment` object, if scope was a `resource_group`."
-    precondition {
-      condition = flatten(var.policy_definitions.*.role_definition_ids) == local.role_definition_ids
-      error_message = "The needed role definitions to assign from azure and calculated locally aren't the same."
-    }
+  precondition {
+    condition     = length(setunion(setsubtract(flatten(var.policy_definitions.*.role_definition_ids), local.role_definition_ids), setsubtract(local.role_definition_ids, flatten(var.policy_definitions.*.role_definition_ids)))) == 0
+    error_message = "The needed role definitions to assign from azure and calculated locally aren't the same."
+  }
 }
