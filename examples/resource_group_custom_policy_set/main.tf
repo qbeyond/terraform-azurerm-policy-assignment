b506ca63fd44ae3954ba3274b32d083c5289ad3c
Thimo Limpert (q.beyond AG) <thimo.limpert@qbeyond.de>
2023-06-09 15:11:11 +0200

    format

--- examples/resource_group_custom_policy_set/main.tf
+++ examples/resource_group_custom_policy_set/main.tf
@@ -5,8 +5,8 @@ provider "azurerm" {
 
 resource "random_pet" "this" {
   separator = ""
-  length = 1
-  prefix = "PolicyAssignment"
+  length    = 1
+  prefix    = "PolicyAssignment"
 }
 
 resource "azurerm_resource_group" "this" {
@@ -20,15 +20,15 @@ resource "azurerm_policy_definition" "this" {
   display_name = "A policy that is just used to test a policy assignment."
   policy_type  = "Custom"
   mode         = "Indexed"
-    policy_rule = jsonencode({
-      if = {
-        field = "type"
-        equals = "qbeyond.Nothing"
-      }
-      then = {
-        effect = "audit"
-      }
-    })
+  policy_rule = jsonencode({
+    if = {
+      field  = "type"
+      equals = "qbeyond.Nothing"
+    }
+    then = {
+      effect = "audit"
+    }
+  })
 }
 
 resource "azurerm_policy_set_definition" "this" {
@@ -38,14 +38,14 @@ resource "azurerm_policy_set_definition" "this" {
 
   policy_definition_reference {
     policy_definition_id = azurerm_policy_definition.this.id
-    parameter_values = jsonencode({})
+    parameter_values     = jsonencode({})
   }
 }
 
 module "policy_assignment_resource_group" {
-  source = "../.."
-  scope = azurerm_resource_group.this.id
-  location = azurerm_resource_group.this.location
+  source                = "../.."
+  scope                 = azurerm_resource_group.this.id
+  location              = azurerm_resource_group.this.location
   policy_set_definition = azurerm_policy_set_definition.this
-  policy_definitions = [azurerm_policy_definition.this]
+  policy_definitions    = [azurerm_policy_definition.this]
 }
